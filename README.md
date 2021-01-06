b62bc337c937d1da9cbc90f837c71c9c636b0f55
ryjjin <66219254+ryjjin@users.noreply.github.com>
2021-01-07 02:01:29 +0300

    2.0.1

--- README.md
+++ README.md
@@ -1,22 +1,45 @@
 # Obsidian-shortcuts-extender
 
-A small plugin that allows you to assign shortcuts to enter special characters - so that you can fully use the markdown markup if you write notes in a language other than English.
+The three key features of the plugin so far are:
+- the ability to change the heading level of the text by pressing the shortcut
+- the ability to clearing the markup of the selected text
+- the ability to assign shortcuts to enter special characters - so that you can fully use the markdown markup if you write notes in a language other than English
+
+## Сhanging the heading level
+
+To change the heading level of the text, just place the cursor on the line whose heading level we want to change.
+Use keyboard shortcuts to specify the heading level:
+- <`Ctrl` + `1`> for heading1
+- <`Ctrl` + `2`> for heading2
+- <...>
+- <`Ctrl` + `6`> for heading6
+
+## Clearing the markup of the selected text
+
+Use `Ctrl` + `0` for clearing the markup of the selected text: only the initial characters of lines are cleared, such as markup characters for headings, numbered and unnumbered lists, quotes/
+
+You can in this way both clear the markup in the drain, where the pointer is at the current moment, and clear the markup of lines from all the selected text.
+
+## Assigning shortcuts to enter special characters
+
+If you are writing notes in a language other than English, you may need to switch layouts frequently to enter special markdown markup characters. This plugin lets you assign you keyboard shortcuts for the most commonly used special characters.
 
 So far, the following keyboard shortcuts have been assigned:
 - `Alt` + `1` - `7` for input `!`, `@`, `#`, `$`, `%`, `^`, `&` symbols
-- `Alt` + `-` ("minus" sign) for input "- " 
-- `Alt` + cyrillic `х` or `ъ` (or EN `[` and `]`) for input []
-- `Alt` + `Shift` + cyrillic `х` or `ъ` (or EN `{` and `}`) for input {}
-- `Alt` + cyrillic `б` or `ю` (or EN `,` and `.`) for input <> 
+- `Alt` + cyrillic `х` or `ъ` (or EN `[` and `]`) for input `[``]`
+- `Alt` + `Shift` + cyrillic `х` or `ъ` (or EN `{` and `}`) for input `{``}`
+- `Alt` + cyrillic `б` or `ю` (or EN `,` and `.`) for input `<``>`
 - `Alt` + cyrillic `ё` for input code fences
-- `Alt` + `Shift` + cyrillic `ё` (or EN "~") for input code fences for python code
-- `Alt` + `Shift` + \ for input '|' symbol
-
-Changed behavior when entering a character through a shortcut `>` and `-`:
-- now if a shortcut is entered, a space after the character is immediately printed,
-- if one or more lines are selected, then all lines will be commented out or change to list item
+- `Alt` + `Shift` + cyrillic `ё` (or EN `~`) for input code fences for python code
+- `Alt` + `Shift` + `\` for input `|` symbol
 
 If you enter brackets or comparison signs or code fences, having previously selected the text, then the text will be enclosed in brackets (in the case of square brackets, a pair will be put at once)
+
+> Notification for mac users:
+>
+> Due to mac keyboard have a lot of build-in shortcuts with `Alt` symbol, many shortcuts need to be rechanged. But also mac users don't need part of shortcuts of this plugin for effective work.
+
+## Additional features
 
 If you enter code fences for python code via Alt + Shift + cyrillic "ё" - new strings will added and result will look as:
 
@@ -24,9 +47,10 @@ If you enter code fences for python code via Alt + Shift + cyrillic "ё" - new s
 <your selected python code>
 ```
 
-## Notification for mac users
+Also, you can use <`Alt` + `-`> for creating list from selected text.
 
-Due to mac keyboard have a lot of build-in shortcuts with `Alt` symbol, many shortcuts need to be rechanged. But also mac users don't need part of shortcuts of this plugin for effective work.
+If the selected text was already a list, the list level will increase by 1, as if you had entered a tab in front of each list item.
+
 
 ## Install notes
 
--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 import { Plugin } from "obsidian";
-import { clipboard } from "electron";
 
 export default class shortcutsExtender extends Plugin {
   async onload() {
@@ -244,6 +243,125 @@ export default class shortcutsExtender extends Plugin {
         },
       ],
     });
+
+  this.addCommand({
+      id: "heading-1",
+      name: "line into level 1 heading",
+      callback: () => this.shortcutHeader1(),
+      hotkeys: [
+        {
+          modifiers: ["Ctrl"],
+          key: "1",
+        },
+      ],
+    });
+
+  this.addCommand({
+      id: "heading-2",
+      name: "line into level 2 heading",
+      callback: () => this.shortcutHeader2(),
+      hotkeys: [
+        {
+          modifiers: ["Ctrl"],
+          key: "2",
+        },
+      ],
+    });
+
+  this.addCommand({
+      id: "heading-3",
+      name: "line into level 3 heading",
+      callback: () => this.shortcutHeader3(),
+      hotkeys: [
+        {
+          modifiers: ["Ctrl"],
+          key: "3",
+        },
+      ],
+    });
+
+  this.addCommand({
+      id: "heading-4",
+      name: "line into level 4 heading",
+      callback: () => this.shortcutHeader4(),
+      hotkeys: [
+        {
+          modifiers: ["Ctrl"],
+          key: "4",
+        },
+      ],
+    });
+
+  this.addCommand({
+      id: "heading-5",
+      name: "line into level 5 heading",
+      callback: () => this.shortcutHeader5(),
+      hotkeys: [
+        {
+          modifiers: ["Ctrl"],
+          key: "5",
+        },
+      ],
+    });
+
+  this.addCommand({
+      id: "heading-6",
+      name: "line into level 6 heading",
+      callback: () => this.shortcutHeader6(),
+      hotkeys: [
+        {
+          modifiers: ["Ctrl"],
+          key: "6",
+        },
+      ],
+    });
+
+  this.addCommand({
+      id: "heading-0",
+      name: "clearing of text formatting",
+      callback: () => this.shortcutHeader0(),
+      hotkeys: [
+        {
+          modifiers: ["Ctrl"],
+          key: "0",
+        },
+      ],
+    });
+  }
+
+  getSelectedText(editor: any) {
+  //thanks to user "Argentina Ortega Sáinz" from the Obsidian community for this feature
+  //For a long time I tried to do without such an approach, which resulted in several bugs and the impossibility of fixing them with non-crutches
+    if (editor.somethingSelected()) {
+      let cursorStart = editor.getCursor(true);
+      let cursorEnd = editor.getCursor(false);
+      let content = editor.getRange(
+        { line: cursorStart.line, ch: 0 },
+        { line: cursorEnd.line, ch: editor.getLine(cursorEnd.line).length }
+      );
+      return {
+        start: { line: cursorStart.line, ch: 0 },
+        end: {
+          line: cursorEnd.line,
+          ch: editor.getLine(cursorEnd.line).length,
+        },
+        content: content,
+      };
+    } else {
+      // Toggle the todo in the line
+      var lineNr = editor.getCursor().line;
+      var contents = editor.getDoc().getLine(lineNr);
+      let cursorStart = {
+        line: lineNr,
+        ch: 0,
+      };
+      let cursorEnd = {
+        line: lineNr,
+        ch: contents.length,
+      };
+      let content = editor.getRange(cursorStart, cursorEnd);
+      return { start: cursorStart, end: cursorEnd, content: content };
+    }
   }
 
   shortcutExclamationMark(): void {
@@ -291,6 +409,15 @@ export default class shortcutsExtender extends Plugin {
     editor.replaceSelection(`\%`);
   }
 
+  shortcutCircumflex(): void {
+    let activeLeaf: any = this.app.workspace.activeLeaf;
+    let editor = activeLeaf.view.sourceMode.cmEditor;
+    let selectedText = editor.somethingSelected()
+      ? editor.getSelection()
+      : false;
+    editor.replaceSelection(`\^`);
+  }
+
   shortcutAmpersand(): void {
     let activeLeaf: any = this.app.workspace.activeLeaf;
     let editor = activeLeaf.view.sourceMode.cmEditor;
@@ -312,29 +439,12 @@ export default class shortcutsExtender extends Plugin {
   }
 
   shortcutGreaterThan(): void {
-    let activeLeaf: any = this.app.workspace.activeLeaf;
+		let activeLeaf: any = this.app.workspace.activeLeaf;
     let editor = activeLeaf.view.sourceMode.cmEditor;
-    let selectedText = editor.somethingSelected()
-      ? editor.getSelection()
-      : false;
-    let resultText = "";
-    if (selectedText) {
-      resultText = selectedText.split('\n').join("\n> ");
-    }
-    editor.replaceSelection(`> ${resultText}`);
-  }
+    let selectedText = this.getSelectedText(editor);
 
-  shortcutListItems(): void {
-    let activeLeaf: any = this.app.workspace.activeLeaf;
-    let editor = activeLeaf.view.sourceMode.cmEditor;
-    let selectedText = editor.somethingSelected()
-      ? editor.getSelection()
-      : false;
-    let resultText = "";
-    if (selectedText) {
-      resultText = selectedText.split('\n').join("\n- ");
-    }
-    editor.replaceSelection(`- ${resultText}`);
+    let resultText = "> " + selectedText.content.split('\n').join("\n> ");
+		editor.replaceRange(resultText, selectedText.start, selectedText.end);
   }
 
   shortcutLeftSquareBracket(): void {
@@ -379,15 +489,6 @@ export default class shortcutsExtender extends Plugin {
     } else editor.replaceSelection(`\``);
   }
 
-  shortcutCircumflex(): void {
-    let activeLeaf: any = this.app.workspace.activeLeaf;
-    let editor = activeLeaf.view.sourceMode.cmEditor;
-    let selectedText = editor.somethingSelected()
-      ? editor.getSelection()
-      : false;
-    editor.replaceSelection(`^`);
-  }
-
   shortcutRightCurlyBracket(): void {
     let activeLeaf: any = this.app.workspace.activeLeaf;
     let editor = activeLeaf.view.sourceMode.cmEditor;
@@ -416,4 +517,121 @@ export default class shortcutsExtender extends Plugin {
       : false;
     editor.replaceSelection(`|`);
   }
+
+  shortcutListItems(): void {
+    let activeLeaf: any = this.app.workspace.activeLeaf;
+    let editor = activeLeaf.view.sourceMode.cmEditor;
+    let selectedText = this.getSelectedText(editor);
+
+    let resultText = "- " + selectedText.content.split('\n').join("\n- ");
+
+    let resultTextCheck = resultText;
+    while (resultTextCheck.search("- - ") >= 0) {
+      resultTextCheck = resultTextCheck.replace("- - ", "	- ");
+    }
+    while (resultTextCheck.search("- 		") >= 0) {
+      resultTextCheck = resultTextCheck.replace("- 		", "	- 	");
+    }
+    while (resultTextCheck.search("- 	- ") >= 0) {
+      resultTextCheck = resultTextCheck.replace("- 	- ", "		- ");
+    }
+    editor.replaceRange(resultTextCheck, selectedText.start, selectedText.end);
+  }
+
+  shortcutHeader0(): void {
+    let activeLeaf: any = this.app.workspace.activeLeaf;
+    let editor = activeLeaf.view.sourceMode.cmEditor;
+    let selectedText = this.getSelectedText(editor);
+
+    var j;
+    let splittedSelectedText = selectedText.content.split(`\n`);
+    for (j in splittedSelectedText) {
+      while (splittedSelectedText[j].substring(0,2) == "##") {
+        splittedSelectedText[j] = splittedSelectedText[j].replace("##", "#");
+      }
+      while (splittedSelectedText[j].substring(0,2) == "# ") {
+        splittedSelectedText[j] = splittedSelectedText[j].substr(2);
+      }
+      while (splittedSelectedText[j].substring(0,2) == "> ") {
+        splittedSelectedText[j] = splittedSelectedText[j].substr(2);
+      }
+      while (splittedSelectedText[j].substring(0,2) == "		") {
+        splittedSelectedText[j] = splittedSelectedText[j].replace("		", "	");
+      }
+      while (splittedSelectedText[j].substring(0,3) == "	- ") {
+        splittedSelectedText[j] = splittedSelectedText[j].substr(3);
+      }
+      while (splittedSelectedText[j].substring(0,2) == "- ") {
+        splittedSelectedText[j] = splittedSelectedText[j].substr(2);
+      }
+      var re_digits = /^\d\.\s/;
+      splittedSelectedText[j] = splittedSelectedText[j].replace(re_digits, "");
+    }
+
+    editor.replaceRange(splittedSelectedText, selectedText.start, selectedText.end);
+  }
+
+  shortcutHeader1(): void {
+    this.shortcutHeader0();
+    let activeLeaf: any = this.app.workspace.activeLeaf;
+    let editor = activeLeaf.view.sourceMode.cmEditor;
+    let selectedText = this.getSelectedText(editor);
+
+    //var clearText = selectedText.content.shortcutHeader0()
+    var resultText = "# " + selectedText.content;
+    editor.replaceRange(resultText, selectedText.start, selectedText.end);
+  }
+
+  shortcutHeader2(): void {
+    this.shortcutHeader0();
+    let activeLeaf: any = this.app.workspace.activeLeaf;
+    let editor = activeLeaf.view.sourceMode.cmEditor;
+    let selectedText = this.getSelectedText(editor);
+
+    var resultText = "## " + selectedText.content;
+    editor.replaceRange(resultText, selectedText.start, selectedText.end);
+  }
+
+  shortcutHeader3(): void {
+    this.shortcutHeader0();
+    let activeLeaf: any = this.app.workspace.activeLeaf;
+    let editor = activeLeaf.view.sourceMode.cmEditor;
+    let selectedText = this.getSelectedText(editor);
+
+    var resultText = "### " + selectedText.content;
+    editor.replaceRange(resultText, selectedText.start, selectedText.end);
+  }
+
+  shortcutHeader4(): void {
+    this.shortcutHeader0();
+    let activeLeaf: any = this.app.workspace.activeLeaf;
+    let editor = activeLeaf.view.sourceMode.cmEditor;
+    let selectedText = this.getSelectedText(editor);
+
+    var resultText = "#### " + selectedText.content;
+    editor.replaceRange(resultText, selectedText.start, selectedText.end);
+  }
+
+  shortcutHeader5(): void {
+    this.shortcutHeader0();
+    let activeLeaf: any = this.app.workspace.activeLeaf;
+    let editor = activeLeaf.view.sourceMode.cmEditor;
+    let selectedText = this.getSelectedText(editor);
+
+    var resultText = "##### " + selectedText.content;
+    editor.replaceRange(resultText, selectedText.start, selectedText.end);
+  }
+
+  shortcutHeader6(): void {
+    this.shortcutHeader0();
+    let activeLeaf: any = this.app.workspace.activeLeaf;
+    let editor = activeLeaf.view.sourceMode.cmEditor;
+    let selectedText = this.getSelectedText(editor);
+
+    var resultText = "###### " + selectedText.content;
+    editor.replaceRange(resultText, selectedText.start, selectedText.end);
+  }
+
+
+
 }
